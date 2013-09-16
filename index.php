6b6d7556c27d59ed524719abfb4d941aff2c0d77
Christopher Morse <christophermorse@fas.harvard.edu>
2013-09-16 10:36:34 -0400

    updated index.php to work with new CSS file

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Christopher Morse</title>
-</head>
+<!DOCTYPE html>
+<html>
 
-<body>
-<p>Project 1 Test Again</p>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Dynamic Web Applications, Project 1</title>
+        <meta name="description" content="Project One by Christopher Morse">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <div id="left"></div>
+        <div id="right"></div>
+        <div id="top"></div>
+        <div id="bottom"></div>
+    </body>
 </html>
+
