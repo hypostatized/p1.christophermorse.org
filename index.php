12f0ef3dc11b3ff488e9468d90961fd79b900393
Christopher Morse <christophermorse@fas.harvard.edu>
2013-09-16 16:49:30 -0400

    additions to layout with links to CSS and HTML validator

--- index.php
+++ index.php
@@ -16,8 +16,10 @@
             <h1>Christopher Morse</h1><br/>
             <img src="chris_orange.png" id="profile" alt="profile">
             <p>I am interested in patterns and systems, symbols and universals, and those places where the boundaries of disciplines blur into new forms. I work in research computing for the humanities at <a href="http://www.darthcrimson.org">Harvard University</a>, and I am taking CSCI E-15 in order to learn how to develop web applications related to my research and personal interests.</p>
-            <p>Presently I live in Cambridge, MA and am a graduate of the <a href="http://hds.harvard.edu">Harvard Divinity School</a> (M.T.S. Religion, Literature & Culture 2009), and the <a href="http://www.umass.edu">University of Massachusetts Amherst</a> (B.A. Japanese Language & Literature 2007). Favorite pastimes include reading, playing cello, and learning foreign languages.</p>
-            <p>I currently use <a href="http://www.brackets.io">Brackets</a> as my preferred text editor.</p>
+            <p>Presently I live in Cambridge, MA and am a graduate of the <a href="http://hds.harvard.edu">Harvard Divinity School</a> (M.T.S. Religion, Literature & Culture 2009), and the <a href="http://www.umass.edu">University of Massachusetts Amherst</a> (B.A. Japanese Language & Literature 2007). Favorite pastimes include reading, cello, gaming of all sorts, learning foreign languages, and <a href="http://www.reddit.com">Reddit</a>.</p>
+            <p>I currently use <a href="http://www.brackets.io">Brackets</a> as my preferred text editor.</p><br/>
+            <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fp1.christophermorse.org%2F"><img src="html.png" id="h_html" alt="html"></a>
+            <a href="http://jigsaw.w3.org/css-validator/validator?uri=p1.christophermorse.org&profile=css3&usermedium=all&warning=1&vextwarning="><img src="css.png" id="c_css" alt="css"></a>
         </div>
     </body>
 </html>
