8c9d5d8de7b987697678d624b8cff90d716610d2
Christopher Morse <christophermorse@fas.harvard.edu>
2013-09-16 11:56:27 -0400

    fixed typo in profile

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <img src="chris_orange.png" id="profile" alt="profile">
             <p>I am interested in patterns and systems, symbols and universals, and those places where the boundaries of disciplines blur into new forms. I work in research computing for the humanities at <a href="http://www.darthcrimson.org">Harvard University</a>, and I am taking CSCI E-15 in order to learn how to develop web applications related to my research and personal interests.</p>
             <p>I am a graduate of the <a href="http://hds.harvard.edu">Harvard Divinity School</a> (M.T.S. Religion, Literature & Culture 2009), and the <a href="http://www.umass.edu">University of Massachusetts Amherst</a> (B.A. Japanese Language & Literature 2007). Favorite pastimes include reading, cello, and learning foreign languages.</p>
-            <p>I am currently use <a href="http://www.brackets.io">Brackets</a> as my preferred text editor.</p>
+            <p>I currently use <a href="http://www.brackets.io">Brackets</a> as my preferred text editor.</p>
         </div>
     </body>
 </html>
