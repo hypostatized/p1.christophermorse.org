4b7ab22b1788ff9ea9a57ec79fbad581e50fc556
Christopher Morse <christophermorse@fas.harvard.edu>
2013-09-16 11:54:02 -0400

    updated index and css with new design

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Dynamic Web Applications, Project 1</title>
         <meta name="description" content="Project One by Christopher Morse">
         <link rel="stylesheet" href="main.css">
@@ -13,6 +12,14 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
+        <div id="content">
+            <h1>Christopher Morse</h1><br/>
+            <img src="chris_orange.png" id="profile" alt="profile">
+            <p>I am interested in patterns and systems, symbols and universals, and those places where the boundaries of disciplines blur into new forms. I work in research computing for the humanities at <a href="http://www.darthcrimson.org">Harvard University</a>, and I am taking CSCI E-15 in order to learn how to develop web applications related to my research and personal interests.</p>
+            <p>I am a graduate of the <a href="http://hds.harvard.edu">Harvard Divinity School</a> (M.T.S. Religion, Literature & Culture 2009), and the <a href="http://www.umass.edu">University of Massachusetts Amherst</a> (B.A. Japanese Language & Literature 2007). Favorite pastimes include reading, cello, and learning foreign languages.</p>
+            <p>I am currently use <a href="http://www.brackets.io">Brackets</a> as my preferred text editor.</p>
+        </div>
     </body>
 </html>
+
 
